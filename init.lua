86c602fd9f488f7d065d53d62281e5c42e6519f8
menisadi <menisadi@gmail.com>
2026-03-04 13:10:56 +0200

    fix: adding neovim library to the lua_ls scope

--- init.lua
+++ init.lua
@@ -81,7 +81,9 @@ vim.lsp.config('lua_ls', {
 	root_markers = { '.luarc.json', '.luarc.jsonc', '.git' },
 	settings = {
 		Lua = {
-			diagnostics = { globals = { 'vim' } },
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
 		},
 	},
 })
