4ba759a7850178701cd4de8ed6d4884fe020af4d
menisadi <menisadi@gmail.com>
2026-03-04 13:08:43 +0200

    fix: make lua_ls now warn again 'vim'

--- init.lua
+++ init.lua
@@ -79,6 +79,11 @@ vim.lsp.config('lua_ls', {
 	cmd = { 'lua-language-server' },
 	filetypes = { 'lua' },
 	root_markers = { '.luarc.json', '.luarc.jsonc', '.git' },
+	settings = {
+		Lua = {
+			diagnostics = { globals = { 'vim' } },
+		},
+	},
 })
 vim.lsp.enable({ "lua_ls", "basedpyright" })
 require('blink.cmp').setup({
