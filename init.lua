5a006b0b9dcd6199430672a3504012f9fb4758b1
menisadi <menisadi@gmail.com>
2026-03-08 00:15:54 +0200

    Adding: conform, flash and aerial

--- init.lua
+++ init.lua
@@ -29,6 +29,9 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+
 vim.pack.add({
 	"https://github.com/tpope/vim-sleuth",
 	"https://github.com/lewis6991/gitsigns.nvim",
@@ -38,6 +41,9 @@ vim.pack.add({
 	"https://github.com/stevearc/oil.nvim",
 	"https://github.com/nvim-treesitter/nvim-treesitter",
 	{ src = "https://github.com/saghen/blink.cmp", version = "1.*" },
+	"https://github.com/stevearc/aerial.nvim",
+	"https://github.com/folke/flash.nvim",
+	"https://github.com/stevearc/conform.nvim",
 })
 
 local wk = require("which-key")
@@ -46,6 +52,7 @@ wk.setup({
 })
 wk.add({
 	{ "<leader>f", group = "[f]zf", icon = "", mode = { "n", "v" } },
+	{ "<leader>l", group = "[L]sp", icon = "󰒕", mode = { "n", "v" } },
 })
 
 require("fzf-lua").setup({ fzf_colors = true })
@@ -55,30 +62,39 @@ vim.keymap.set("n", "<leader>fg", fzf.live_grep, { desc = "Fzf live grep" })
 vim.keymap.set("n", "<leader>fb", fzf.builtin, { desc = "Fzf Builtin Picker" })
 
 require("oil").setup()
-local ts_parsers =
-	{
-		"python", "bash", "gitcommit", "gitignore",
-		"json", "lua", "dockerfile", "markdown",
-		"sql", "toml", "yaml"
-	}
+vim.keymap.set("n", "<leader>lO", "<cmd>Oil<cr>", { desc = "Open Oil" })
+vim.keymap.set("n", "-", "<cmd>Oil<cr>", { desc = "Open parent directory" })
+
+local ts_parsers = {
+	"python",
+	"bash",
+	"gitcommit",
+	"gitignore",
+	"json",
+	"lua",
+	"dockerfile",
+	"markdown",
+	"sql",
+	"toml",
+	"yaml",
+}
 local nts = require("nvim-treesitter")
 nts.install(ts_parsers)
-vim.lsp.config('basedpyright', {
-	cmd = { 'basedpyright-langserver', '--stdio' },
+vim.lsp.config("basedpyright", {
+	cmd = { "basedpyright-langserver", "--stdio" },
 	settings = {
 		analysis = {
 			autoSearchPaths = true,
 			diagnosticMode = "openFilesOnly",
-
-		}
+		},
 	},
-	filetypes = { 'python' },
-	root_markers = { 'pyproject.toml', 'setup.py', 'setup.cfg', 'requirements.txt', '.git' },
+	filetypes = { "python" },
+	root_markers = { "pyproject.toml", "setup.py", "setup.cfg", "requirements.txt", ".git" },
 })
-vim.lsp.config('lua_ls', {
-	cmd = { 'lua-language-server' },
-	filetypes = { 'lua' },
-	root_markers = { '.luarc.json', '.luarc.jsonc', '.git' },
+vim.lsp.config("lua_ls", {
+	cmd = { "lua-language-server" },
+	filetypes = { "lua" },
+	root_markers = { ".luarc.json", ".luarc.jsonc", ".git" },
 	settings = {
 		Lua = {
 			workspace = {
@@ -88,8 +104,40 @@ vim.lsp.config('lua_ls', {
 	},
 })
 vim.lsp.enable({ "lua_ls", "basedpyright" })
-require('blink.cmp').setup({
+require("blink.cmp").setup({
 	fuzzy = { implementation = "lua" },
 	keymap = { preset = "default" },
 	sources = { default = { "lsp", "path", "snippets", "buffer" } },
 })
+
+require("conform").setup({
+	format_on_save = true,
+	formatters_by_ft = {
+		lua = { "stylua" },
+		python = { "ruff" },
+	},
+})
+vim.keymap.set("n", "<leader>lf", require("conform").format, { desc = "Format buffer" })
+
+require("flash").setup({
+	modes = {
+		search = {
+			enabled = false,
+		},
+		char = {
+			jump_labels = false,
+			-- highlight = { backdrop = false, matches = true },
+			multi_line = false,
+		},
+	},
+})
+vim.keymap.set("n", "s", require("flash").jump, { desc = "Flash jump" })
+vim.keymap.set("n", "S", require("flash").treesitter, { desc = "Flash jump" })
+
+require("aerial").setup({
+	layout = {
+		max_width = { 40, 0.3 },
+		default_direction = "prefer_left",
+	},
+})
+vim.keymap.set("n", "<leader>la", "<cmd>AerialToggle<CR>", { desc = "Aerial Toggle" })
