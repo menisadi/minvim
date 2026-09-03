9e498ef4d63e34eb902dc4f4fb0f868c1baaaf4a
menisadi <menisadi@gmail.com>
2026-09-03 13:03:51 +0300

    feat(theme): tune kanagawa lotus palette for contrast and hue separation
    Darkens/saturates syntax accent colors to clear WCAG AA (4.5:1) against
    the custom Fuji-derived background, and rotates comment/identifier hues
    away from the warm background to avoid hue-crowding.

--- init.lua
+++ init.lua
@@ -50,6 +50,36 @@ vim.pack.add({
 	"https://github.com/rebelot/kanagawa.nvim",
 	"https://github.com/zk-org/zk-nvim",
 })
+require("kanagawa").setup({
+	colors = {
+		palette = {
+			lotusInk1 = "#3b5159",
+			lotusWhite0 = "#b7bfb0",
+			lotusWhite1 = "#f1dcb7",
+			lotusWhite2 = "#f2ecd1",
+			lotusWhite3 = "#fcf8ed",
+			lotusWhite4 = "#d5ded4",
+			lotusWhite5 = "#d5ceb4",
+			lotusGray3 = "#61707b",
+			lotusGreen = "#60726a",
+			lotusAqua = "#5d7277",
+			lotusBlue4 = "#3a6a85",
+			lotusBlue5 = "#275071",
+			lotusViolet4 = "#1a3959",
+			lotusRed = "#a94d36",
+			lotusOrange = "#a9523e",
+			lotusPink = "#a75939",
+			lotusYellow = "#5c732f",
+			lotusYellow2 = "#915f4e",
+			lotusTeal1 = "#4c7988",
+			lotusTeal2 = "#497d96",
+			lotusRed3 = "#55463e",
+			lotusOrange2 = "#7c645a",
+			lotusTeal3 = "#4d636a",
+			lotusAqua2 = "#547379",
+		},
+	},
+})
 vim.cmd([[colorscheme kanagawa]])
 
 _G.hebrew_status = function()
